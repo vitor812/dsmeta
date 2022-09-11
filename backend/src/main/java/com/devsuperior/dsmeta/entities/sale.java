155e418c02004e27ea62ce8e570a76008f5818b4
vitor812 <vitorventura1q@gmail.com>
2022-09-11 17:54:21 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/entities/sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/sale.java
@@ -11,64 +11,64 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
- private long id;
- private String sellerName;
- private Integer visited;
- private Integer deals;
-  private Double amount;
- private LocalDate date;
- 
- public Sale() {
-	 }
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+	}
 
-public long getId() {
-	return id;
-}
+	public Long getId() {
+		return id;
+	}
 
-public void setId(long id) {
-	this.id = id;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public String getSellerName() {
-	return sellerName;
-}
+	public String getSellerName() {
+		return sellerName;
+	}
 
-public void setSellerName(String sellerName) {
-	this.sellerName = sellerName;
-}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
 
-public Integer getVisited() {
-	return visited;
-}
+	public Integer getVisited() {
+		return visited;
+	}
 
-public void setVisited(Integer visited) {
-	this.visited = visited;
-}
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
 
-public Integer getDeals() {
-	return deals;
-}
+	public Integer getDeals() {
+		return deals;
+	}
 
-public void setDeals(Integer deals) {
-	this.deals = deals;
-}
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
 
-public Double getAmount() {
-	return amount;
-}
+	public Double getAmount() {
+		return amount;
+	}
 
-public void setAmount(Double amount) {
-	this.amount = amount;
-}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
-public LocalDate getDate() {
-	return date;
-}
+	public LocalDate getDate() {
+		return date;
+	}
 
-public void setDate(LocalDate date) {
-	this.date = date;
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 }
- }
